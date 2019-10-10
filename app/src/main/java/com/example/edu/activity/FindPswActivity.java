b423eb043741f901b9762b085dabcd96c0df80c6
gpf <1040316431@QQ.com>
2019-10-10 20:31:01 +0800

    登录界面

--- app/src/main/java/com/example/edu/activity/FindPswActivity.java
+++ app/src/main/java/com/example/edu/activity/FindPswActivity.java
@@ -1,14 +1,16 @@
-package com.example.edu;
+package com.example.edu.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.edu.R;
+
+public class FindPswActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_find_psw);
     }
 }
---
+++ app/src/main/java/com/example/edu/activity/LoginActivity.java
@@ -0,0 +1,91 @@
+package com.example.edu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.edu.R;
+
+public class LoginActivity extends AppCompatActivity {
+    //快捷登录
+    private ImageView qq,wx;
+    //返回键,显示的注册，找回密码
+    private TextView tv_register,tv_find_psw;
+    //登录按钮
+    private Button btn_login;
+    //获取的用户名，密码
+    private String userName,psw;
+    //用户名和密码的输入框
+    private EditText et_user_name,et_psw;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //设置此界面为竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+    }
+
+    //获取界面控件
+    private void init() {
+        //从activity_login.xml中获取的
+        tv_register=findViewById(R.id.tv_register);
+        tv_find_psw=findViewById(R.id.tv_find_psw);
+        btn_login=findViewById(R.id.btn_login);
+        et_user_name=findViewById(R.id.et_user_name);
+        et_psw=findViewById(R.id.et_psw);
+        qq = findViewById(R.id.login_qq);
+        wx = findViewById(R.id.login_chat);
+        //立即注册控件的点击事件
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //为了跳转到注册界面，并实现注册功能
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+        //找回密码控件的点击事件
+        tv_find_psw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到找回密码界面
+                Intent intent=new Intent(LoginActivity.this,FindPswActivity.class);
+                startActivity(intent);
+            }
+        });
+        //登录按钮的点击事件
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //快捷登录点击事件
+        qq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        wx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+}
